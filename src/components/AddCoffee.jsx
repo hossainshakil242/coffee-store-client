8c9eb2f94760772846d1c4b7d5f6f55e7eab7664
hossainshakil242 <hossainshakil242@gmail.com>
2023-09-02 13:49:04 +0600

    sent data from client side to server site

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,9 +1,41 @@
 const AddCoffee = () => {
+
+    const handleAddCoffee = e =>{
+        e.preventDefault();
+
+        const form = e.target;
+
+        const name = form.name.value;
+        const quantity = form.quantity.value;
+        const supplier = form.supplier.value;
+        const taste = form.taste.value;
+        const category = form.category.value;
+        const details = form.details.value;
+        const photo = form.photo.value;
+
+        const newCoffee = {name,quantity,supplier,taste,category,details,photo};
+        console.log(newCoffee);
+
+        // send data to the server
+        fetch(`http://localhost:5000/coffee`, {
+            method: 'POST',
+            headers: {
+                'content-type' : 'application/json'
+            },
+            body: JSON.stringify(newCoffee)
+        })
+        .then(res=> res.json())
+        .then(data=>{
+            console.log(data);
+        })
+     
+    }
+
     return (
         <div className="bg-[#F4F3F0] p-24 ">
             <h2 className="text-2xl font-bold">Add Coffee</h2>
 
-            <form>
+            <form onSubmit={handleAddCoffee}>
                 {/* form name and quantity row */}
                 <div  className="md:flex gap-5">
                     <div className="form-control md:w-1/2">
