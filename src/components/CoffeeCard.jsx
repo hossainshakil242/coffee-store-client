fe2606e7c590dbe13341a287189a112b5ee20523
hossainshakil242 <hossainshakil242@gmail.com>
2023-09-03 13:06:13 +0600

    deleted data and add confirmation popUP

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,8 +1,58 @@
+import { data } from "autoprefixer";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const CoffeeCard = ({ coffee }) => {
 
     const { _id, name, quantity, supplier, taste, category, details, photo } = coffee;
+
+    const handleDelete = (_id) => {
+        const swalWithBootstrapButtons = Swal.mixin({
+            customClass: {
+                confirmButton: 'btn btn-success',
+                cancelButton: 'btn btn-danger'
+            },
+            buttonsStyling: false
+        })
+
+        swalWithBootstrapButtons.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Yes, delete it!',
+            cancelButtonText: 'No, cancel!',
+            reverseButtons: true
+        }).then((result) => {
+            if (result.isConfirmed) {
+                
+                fetch(`http://localhost:5000/coffee/${_id}`, {
+                    method: "DELETE"
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount > 0) {
+                            swalWithBootstrapButtons.fire(
+                                'Deleted!',
+                                'Your file has been deleted.',
+                                'success'
+                            )
+                        }
+                    })
+
+            } else if (
+                /* Read more about handling dismissals below */
+                result.dismiss === Swal.DismissReason.cancel
+            ) {
+                swalWithBootstrapButtons.fire(
+                    'Cancelled',
+                    'Your imaginary file is safe :)',
+                    'error'
+                )
+            }
+        })
+    }
+
     return (
         <div className="card grid grid-rows-1 md:grid-cols-2 card-side bg-base-100 shadow-xl">
             <figure><img src={photo} className="w-full" alt={category} /></figure>
@@ -11,7 +61,7 @@ const CoffeeCard = ({ coffee }) => {
                 <p>Click the button to watch on Jetflix app.</p>
                 <div className="card-actions justify-center">
                     <button className="btn btn-secondary btn-outline">Buy</button>
-                    <button className="btn btn-secondary btn-outline">Delete</button>
+                    <button onClick={() => handleDelete(_id)} className="btn btn-secondary btn-outline">Delete</button>
                     <Link to={`/updateCoffee/${_id}`}>
                         <button className="btn btn-secondary btn-outline">Edit</button>
                     </Link>
